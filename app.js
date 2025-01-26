5c6fd93fa6381f7f9dd26c88d5112faafff1bc1e
leandrofmoraes <leandrofmraes@gmail.com>
2025-01-26 17:51:38 -0300

    Função para Sortear amigo, recurso para adicionar elemento li com nomes ao Html, função para validar entrada

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let names = new Array();
-const addButton = document.querySelector('[data-button-add]');
+const nomes = new Array();
+const botaoAdd = document.querySelector('[data-botao-add]');
+const botaoSortear = document.querySelector('[data-botao-sortear]');
+const resultado = document.querySelector('[data-resultado]');
 
-function adicionarNome(){
-    let name = document.querySelector('[data-name]').value;
-    names.push(name);
-    console.log(names);
+function adicionarAmigo(){
+    const nome = document.querySelector('[data-nome]').value.trim();
+    const lista = document.querySelector('[data-nomes-lista]');
+
+    // Se for um nome válido, adiciona ao array nomes.
+    validarEntrada(nome) ? nomes.push(nome) : alert('Digite um nome válido');
+
+    const elementListItem = document.createElement('li'); // criar novo elemento <li></li>
+    elementListItem.innerHTML += nome; // <li>nome</li>
+    lista.appendChild(elementListItem); // Adiciona o nome na lista de amigos
 }
 
-addButton.addEventListener('click', () => adicionarNome());
+function sortearAmigo(){
+    return parseInt(Math.random() * nomes.length);
+}
+
+function validarEntrada(entrada){
+    //Valida se a entrada é vazia ou se é um número
+    if(entrada.length === 0 || !isNaN(entrada)){
+        return false;
+    }
+    return true;
+}
+
+botaoAdd.addEventListener('click', () => adicionarAmigo());
+botaoSortear.addEventListener('click', () => sortearAmigo());
