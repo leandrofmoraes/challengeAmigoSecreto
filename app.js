24892919d225d94660c5b04ac064bc905c758be6
leandrofmoraes <leandrofmraes@gmail.com>
2025-01-29 20:42:37 -0300

    Alterar botão dinâmicamente e outras melhorias de código

--- app.js
+++ app.js
@@ -17,61 +17,82 @@
  * Tecnologias: JavaScript, HTML, CSS
  */
 
-const nomes = new Set();
+// Variáveis globais
+const amigos = new Set();
 
 const botaoAdd = document.querySelector('[data-botao-add]');
 const botaoSortear = document.querySelector('[data-botao-sortear]');
+const lista = document.querySelector('[data-nomes-lista]');
+const resultado = document.querySelector('[data-resultado]');
+
+alternarBotaoSortear();
 
 function adicionarAmigo(){
     const campoNome = document.querySelector('[data-nome]');
-    const lista = document.querySelector('[data-nomes-lista]');
 
-    // Pegar o valor do campo de texto e remover espaços em branco
+    resultado.innerHTML = ''; // Limpa o resultado anterior
+
+    // Pegar o valor do campo de texto e remove espaços em branco
     const nome = campoNome.value.trim();
 
-    // Se for um nome válido, adiciona ao array nomes.
-    // validarEntrada(nome) ? nomes.add(nome) : alert(mensagem);
-    nomes.add(nome);
+    if(!validarEntrada(nome)){
+        alert('Digite um nome válido');
+        return;
+    }
 
-    const elementListItem = document.createElement('li'); // criar novo elemento <li></li>
-    elementListItem.innerHTML += nome; // <li>nome</li>
-    lista.appendChild(elementListItem); // Adiciona o nome na lista de amigos
+    amigos.add(nome);
 
+    atualizarLista();
+    alternarBotaoSortear();
     campoNome.value = ''; // Limpa o campo de texto
 }
 
 function exibirResultado(){
-    const resultado = document.querySelector('[data-resultado]');
 
     const elementListItem = document.createElement('li');
     elementListItem.innerHTML += `Amigo sorteado: ${sortearAmigo()}`;
+
+    lista.innerHTML = ''; // Limpa a lista de amigos
+    amigos.clear(); // Limpa o Set de amigos
 
     resultado.appendChild(elementListItem);
 }
 
 function sortearAmigo(){
-    const nomesArray = Array.from(nomes); // Converte o Set para Array
+    const amigosArray = Array.from(amigos); // Converte o Set para Array
 
-    //sortear um número entre 0 e o tamanho do array de nomes
-    const indice = Math.floor(Math.random() * nomes.length);
+    //sortear um número entre 0 e o tamanho do array de amigos
+    const indice = Math.floor(Math.random() * amigos.size);
 
-    return nomesArray[indice];
+    return amigosArray[indice];
 }
 
-function validarEntrada(nome){
+function validarEntrada(entrada){
 
     //Não é vazio ou não é um número
-    if(nome.length > 0 || isNaN(nome)){
-        alert('Digite um nome válido');
-        return;
-    }
+    return entrada.length > 0 || isNaN(entrada);
+}
 
-    if(nomes.has(nome)) {
-        alert(`O nome ${nome} já foi adicionado.`);
-        return;
-    }
+function atualizarLista(){
+    lista.innerHTML = ''; // Limpa a lista de amigos
 
-    return true;
+    //Percorre o Set de amigos e adiciona cada nome ao Html
+    amigos.forEach((amigo) => {;
+        const elementListItem = document.createElement('li'); // criar novo elemento <li></li>
+        elementListItem.innerHTML += amigo; // <li>nome</li>
+        lista.appendChild(elementListItem); // Adiciona o nome na lista de amigos
+    });
+}
+
+// Alterna a aparência do botão sortear
+function alternarBotaoSortear(){
+
+    const ativo = amigos.size > 1 // Há mais de um nome na lista?
+
+    botaoSortear.disabled = !ativo;
+    botaoSortear.style.backgroundColor = ativo ? "" : "#C4C4C4";
+    botaoSortear.style.color           = ativo ? "" : "#444444";
+    botaoSortear.style.cursor          = ativo ? "" : "default";
 }
 
 botaoAdd.addEventListener('click', () => adicionarAmigo());
